1bc98516ffbadfe94f012db2e25fb2e98f687585
magilo <53962625+magilo@users.noreply.github.com>
2022-02-03 00:55:10 -0500

    refactor spend route with SpendService

--- src/routes/SpendRouter.js
+++ src/routes/SpendRouter.js
@@ -4,17 +4,15 @@ const router = express.Router();
 const { Partner, Transaction } = require('../models');
 const sequelize = require('../database');
 const { Op } = require("sequelize");
-const { getUserBalance } = require('./UserService')
+const { spendPoints } = require('./SpendService');
 
 
 router.patch('/api/user/spend', async (req, res, next) => {
   try {
 
     let pointsToSpend = req.body.points;
-    //also where leftover doesn't occur
-    // const orderByTimestamp = await Transaction.findAll({
-    //   order: sequelize.col('timestamp')
-    // })
+
+    /**grab transactions in timestamp order where there are leftover points**/
     const orderByTimestamp = await Transaction.findAll({
       where: {
         leftover: {
@@ -23,20 +21,19 @@ router.patch('/api/user/spend', async (req, res, next) => {
       },
       order: sequelize.col('timestamp')
     })
-    // console.log('orderByTimestamp', orderByTimestamp)
 
     const userBalance = await Partner.sum('points');
-    // console.log('userBalance', userBalance)
 
+    /**handle cases where user has no points/transactions
+     * or doesn't have enough points for the spend call
+     */
     if (userBalance === null) {
-      // console.log(userBalance, pointsToSpend)
       return res.json(
         {
           message: "no points available",
           balance: 0
         })
     } else if (userBalance < pointsToSpend) {
-      // console.log(userBalance, pointsToSpend)
       return res.json(
         {
           message: "not enough points",
@@ -44,60 +41,10 @@ router.patch('/api/user/spend', async (req, res, next) => {
         });
     }
 
-    const activePayers = {} //payers that paid out for this spend call
-    let timestampIdx = 0; //index for orderByTimestamp
+    /**can proceed to spend points as normal**/
+    const payerList = await spendPoints(orderByTimestamp, pointsToSpend);
 
-    while (pointsToSpend > 0) {
-      let currentTransaction = orderByTimestamp[timestampIdx];
-      let currentPayer = currentTransaction.payer;
-
-      const payerBalance = await Partner.findOne({ where: { payer: currentPayer } })
-      //initialize payer in activePayers
-      if (!(currentPayer in activePayers)) activePayers[currentPayer] = 0;
-
-      //first use up any leftover points in the currentTransaction
-      // then go through each transaction until pointsToSpend is paid out
-      if (currentTransaction.leftover >= pointsToSpend) {
-        //this means that points to spend will go to 0
-        //there may or may not be leftover points
-        activePayers[currentPayer] -= pointsToSpend;
-
-        payerBalance.points -= pointsToSpend;
-        await payerBalance.save();
-
-        currentTransaction.leftover -= pointsToSpend;
-        await currentTransaction.save();
-
-        pointsToSpend -= pointsToSpend; //0
-      }
-      else if (currentTransaction.leftover < pointsToSpend) {
-        //this means it will use up all of the points in the currentTransaction
-        //there will still be pointsToSpend after this
-        activePayers[currentPayer] -= currentTransaction.leftover;
-
-        payerBalance.points -= currentTransaction.leftover;
-        await payerBalance.save();
-
-        pointsToSpend -= currentTransaction.leftover
-
-        currentTransaction.leftover = 0;
-        await currentTransaction.save();
-      }
-
-      // console.log('payerBalance', payerBalance)
-      // console.log('leftover points', currentTransaction.leftover)
-      // console.log('pointsToSpend', pointsToSpend)
-
-      timestampIdx++;
-    }
-
-    let paidList = []
-    for (let payerName in activePayers) {
-      const paidAmount = { payer: payerName, points: activePayers[payerName] }
-      paidList.push(paidAmount);
-    }
-
-    res.status(200).send(paidList)
+    res.status(200).send(payerList)
 
   } catch (err) {
     next(err);
--- src/routes/SpendService.js
+++ src/routes/SpendService.js
@@ -2,10 +2,62 @@ const { Partner, Transaction } = require('../models');
 const sequelize = require('../database');
 const { Op } = require("sequelize");
 
-const spendPoints = async (???) => {
+const spendPoints = async (orderByTimestamp, pointsToSpend) => {
 
+  const activePayers = {} //payers that paid out for this spend call
+  let timestampIdx = 0; //index for orderByTimestamp
 
-  return activePayers;
+  while (pointsToSpend > 0) {
+    let currentTransaction = orderByTimestamp[timestampIdx];
+    let currentPayer = currentTransaction.payer;
+
+    const payerBalance = await Partner.findOne({ where: { payer: currentPayer } })
+    //initialize payer in activePayers
+    if (!(currentPayer in activePayers)) activePayers[currentPayer] = 0;
+
+    //first use up any leftover points in the currentTransaction
+    // then go through each transaction until pointsToSpend is paid out
+    if (currentTransaction.leftover >= pointsToSpend) {
+      //this means that points to spend will go to 0
+      //there may or may not be leftover points
+      activePayers[currentPayer] -= pointsToSpend;
+
+      payerBalance.points -= pointsToSpend;
+      await payerBalance.save();
+
+      currentTransaction.leftover -= pointsToSpend;
+      await currentTransaction.save();
+
+      pointsToSpend -= pointsToSpend; //0
+    }
+    else if (currentTransaction.leftover < pointsToSpend) {
+      //this means it will use up all of the points in the currentTransaction
+      //there will still be pointsToSpend after this
+      activePayers[currentPayer] -= currentTransaction.leftover;
+
+      payerBalance.points -= currentTransaction.leftover;
+      await payerBalance.save();
+
+      pointsToSpend -= currentTransaction.leftover
+
+      currentTransaction.leftover = 0;
+      await currentTransaction.save();
+    }
+
+    // console.log('payerBalance', payerBalance)
+    // console.log('leftover points', currentTransaction.leftover)
+    // console.log('pointsToSpend', pointsToSpend)
+
+    timestampIdx++;
+  }
+
+  let paidList = []
+  for (let payerName in activePayers) {
+    const paidAmount = { payer: payerName, points: activePayers[payerName] }
+    paidList.push(paidAmount);
+  }
+
+  return paidList;
 }
 
 
