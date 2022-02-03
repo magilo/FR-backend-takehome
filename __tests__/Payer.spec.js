425febdd3680022ce0f1dfcd223b7a13f6144ad9
magilo <53962625+magilo@users.noreply.github.com>
2022-02-03 01:32:26 -0500

    add describe block to payer tests

--- __tests__/Payer.spec.js
+++ __tests__/Payer.spec.js
@@ -46,48 +46,47 @@ const putTransaction3 = () => {
     })
 }
 
-it('creates new payer if payer does not exist in table', async () => {
-  const response = await putTransaction1();
-  expect(response.status).toBe(200);
-  expect(response.body.message).toBe('new payer added');
-})
+describe('add transaction route', () => {
+  it('creates new payer if payer does not exist in Partner table', async () => {
+    const response = await putTransaction1();
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('new payer added');
+  })
 
 
-it('updates payer points if payer already exists', async () => {
-  // const response = await Promise.all([
-  //   putTransaction1(),
-  //   putTransaction3()
-  // ]);
+  it('updates payer points if payer already exists', async () => {
+    await putTransaction1();
+    await putTransaction2();
+    const response = await putTransaction3();
 
-  await putTransaction1();
-  await putTransaction2();
-  const response = await putTransaction3();
+    // console.log('response', response.body)
+    const updatedPayer = await Partner.findOne({ where: { payer: "DANNON" } })
 
-  console.log('response', response.body)
-  const updatedPayer = await Partner.findOne({ where: { payer: "DANNON" } })
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('points updated');
+    expect(updatedPayer.points).toBe(800);
+  })
 
-  expect(response.status).toBe(200);
-  expect(response.body.message).toBe('points updated');
-  expect(updatedPayer.points).toBe(800);
-})
+  it('adds transactions to the transaction table', async () => {
+    await Promise.all([
+      putTransaction1(),
+      putTransaction3()
+    ]);
 
-it('adds transactions to the transaction table', async () => {
-  await Promise.all([
-    putTransaction1(),
-    putTransaction3()
-  ]);
+    const transactionList = await Transaction.findAll();
+    expect(transactionList.length).toBe(2);
+  })
 
-  const transactionList = await Transaction.findAll();
-  expect(transactionList.length).toBe(2);
-})
+  it('saves the payer, points, timestamp to the transactions table', async () => {
+    await putTransaction1();
+    const transactionList = await Transaction.findAll();
+    const savedTransaction = transactionList[0];
+    expect(savedTransaction.payer).toBe('DANNON');
+    expect(savedTransaction.points).toBe(1000);
+    expect(savedTransaction.timestamp).toBe("2020-11-02T14:00:00Z");
+    expect(savedTransaction.leftover).toBe(1000);
+  })
 
-it('saves the payer, points, timestamp to the transactions table', async () => {
-  await putTransaction1();
-  const transactionList = await Transaction.findAll();
-  const savedTransaction = transactionList[0];
-  expect(savedTransaction.payer).toBe('DANNON');
-  expect(savedTransaction.points).toBe(1000);
-  expect(savedTransaction.timestamp).toBe("2020-11-02T14:00:00Z");
-  expect(savedTransaction.leftover).toBe(1000);
-})
+});
+
 
