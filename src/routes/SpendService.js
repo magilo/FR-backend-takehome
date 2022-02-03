c9d0709e94b4596497ef73c74472ca3fec11e99c
magilo <53962625+magilo@users.noreply.github.com>
2022-02-03 00:39:34 -0500

    create SpendService file for spend route

---
+++ src/routes/SpendService.js
@@ -0,0 +1,14 @@
+const { Partner, Transaction } = require('../models');
+const sequelize = require('../database');
+const { Op } = require("sequelize");
+
+const spendPoints = async (???) => {
+
+
+  return activePayers;
+}
+
+
+module.exports = {
+  spendPoints
+}
