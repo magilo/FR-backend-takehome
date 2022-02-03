3bf54bd9ac4ec6c9ffbb7dd6ed4b0022f107d7c7
magilo <53962625+magilo@users.noreply.github.com>
2022-02-03 01:40:33 -0500

    deleted unused code in app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,11 @@ const express = require('express');
 const app = express();
 const TransactionRouter = require('./routes/TransactionRouter');
 const SpendRouter = require('./routes/SpendRouter');
-// const PointsRouter = require('./routes');
 
 
 app.use(express.json());
 
-// app.use('/', PointsRouter);
 app.use(TransactionRouter);
 app.use(SpendRouter);
-// app.use('/api/transactions', TransactionRouter);
-// app.use('/api/user/spend', require('./routes/SpendRouter'));
 
 module.exports = app;
