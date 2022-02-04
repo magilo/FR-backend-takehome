6bbd567f083888fb4d28d771c0d9d7a521c62bcf
magilo <53962625+magilo@users.noreply.github.com>
2022-02-04 01:20:05 -0500

    refactored transaction route with transaction service

---
+++ src/routes/TransactionService.js
@@ -0,0 +1,33 @@
+const { Partner, Transaction } = require('../models');
+
+const addTransaction = async (body, payerName) => {
+
+  const transaction = await Transaction.create({
+    payer: body.payer,
+    points: body.points,
+    timestamp: body.timestamp,
+    leftover: body.points
+  })
+
+  /* create or update payer points */
+  const payer = await Partner.findOne({ where: { payer: payerName } })
+  if (payer === null) {
+    await Partner.create({
+      payer: body.payer,
+      points: body.points,
+    })
+    const newPayer = await Partner.findOne({ where: { payer: payerName } })
+    await transaction.setPartner(newPayer);
+    return { message: "new payer added" };
+  } else {
+    payer.points += body.points;
+    await payer.save();
+    await transaction.setPartner(payer);
+    return { message: "points updated" };
+  }
+}
+
+
+module.exports = {
+  addTransaction
+};
