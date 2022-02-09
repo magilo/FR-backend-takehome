5e8def6947331b1127e44557d5c8d0400728e310
magilo <53962625+magilo@users.noreply.github.com>
2022-02-08 23:14:06 -0500

    swagger setup working

--- src/app.js
+++ src/app.js
@@ -2,6 +2,28 @@ const express = require('express');
 const app = express();
 const Routers = require('./routes')
 
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'MG Rewards API project',
+      version: '1.0.0'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/'
+      }
+    ]
+  },
+  apis: ['./app.js']
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(express.json());
 
 /* get all routes from the routes index file */
