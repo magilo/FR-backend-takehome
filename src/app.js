107e88cf295b2c4e3ef73035ec4e79931551160a
magilo <53962625+magilo@users.noreply.github.com>
2022-02-08 23:41:00 -0500

    updated app.use error handling endware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,13 @@ const Routers = require('./routes')
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
+/* express middleware */
+app.use(express.json());
+
+/* get all routes from the routes index file */
+app.use(Routers)
+
+/* options for swaggerJSDoc */
 const options = {
   definition: {
     openapi: '3.0.0',
@@ -24,9 +31,21 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use(express.json());
+//send 404 for any remaining requests
+app.use((req, res, next) => {
+  const error = new Error('404 Not Found')
+  error.status = 404
+  next(error)
+})
 
-/* get all routes from the routes index file */
-app.use(Routers)
+// // error handling endware
+// app.use((err, req, res, next) => {
+//   console.error(err)
+//   console.error(err.stack)
+//   res.status(err.status || 500).send(err.message || 'Internal server error.')
+// })
+
+
+
 
 module.exports = app;
