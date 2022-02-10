43508cb637ad68cd961cac4b82b122793260da90
magilo <53962625+magilo@users.noreply.github.com>
2022-02-10 00:24:31 -0500

    pointsBalance route documentation

--- src/routes/PointsBalanceRouter.js
+++ src/routes/PointsBalanceRouter.js
@@ -4,33 +4,6 @@ const router = express.Router();
 const { Partner } = require('../models');
 
 
-
-/**
- * @swagger
- * /hello:
- *   get:
- *     description: Returns the homepage
- *     responses:
- *       200:
- *         description: hello world
- */
-
-router.get('/hello', (req, res) => {
-  res.send('Hello World (Version 2)!');
-});
-
-/**
- * @swagger
- * /api/user/balance:
- *   get:
- *     summary: Gets a list of payers and their points balance.
- *     description: A user's balance is made up of the points balance from the partners that they had a transaction with.
- *     responses:
- *       200:
- *         description: OK
- */
-
-
 router.get('/api/user/balance', async (req, res, next) => {
   try {
     const pointsBalance = await Partner.findAll({
